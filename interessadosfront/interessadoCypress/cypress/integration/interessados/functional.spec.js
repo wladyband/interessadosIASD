7d185afc5e4d4c8c79129019c7b980db54ce63e9
Wladimir Bandeira <tecnico100@gmail.com>
2022-01-10 16:55:51 -0300

    realizando construção de testes iniciais

---
+++ interessadosfront/interessadoCypress/cypress/integration/interessados/functional.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+
+describe('acesso padrao',  () => {
+
+    before(() => {
+        cy.visit('http://localhost:4200/discipulos')
+    })
+
+    it('teste qualquer', () => {
+        
+    })
+})
