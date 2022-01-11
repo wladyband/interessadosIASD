fd90c2cb9fc3c843a594ba81b5d9fee5ecacbc3c
Wladimir Bandeira <tecnico100@gmail.com>
2022-01-11 15:46:32 -0300

    criando comandos

--- interessadosfront/interessadoCypress/cypress/integration/interessados/functional.spec.js
+++ interessadosfront/interessadoCypress/cypress/integration/interessados/functional.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 import loc from '../../support/locators'
+import '../../support/commandsDiscipulo'
 
 describe('acesso padrao',  () => {
 
@@ -20,22 +21,11 @@ describe('acesso padrao',  () => {
         cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_FLAVIA).click()
         cy.get(loc.CADASTRAR_DISCUPULOS.VARIFICAR_FLAVIA_COMO_SELECIONADO).should('contain','Flávia')
     })
+    
 
     it('salvando discipulo', () => {
         cy.visit(loc.URL.CADASTRO_DISCIPULOS)
-        cy.get(loc.CADASTRAR_DISCUPULOS.CLICAR_NO_CALENDARIO).click() 
-        cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_DATA).click()
-        cy.get(loc.CADASTRAR_DISCUPULOS.ESCREVER_NOME_DO_DISCIPULO).type('Gildo teste LT') 
-        cy.get(loc.CADASTRAR_DISCUPULOS.ESCREVER_O_ENDERECO_DO_DISCIPULO).type('Rua João adinaldo da Cunha')
-        cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_TIPO_INTERESSE).click()
-        cy.get(loc.CADASTRAR_DISCUPULOS.CLICAR_NO_TIPO_INTERESSE).click()
-        cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_TIPO_ATENDIMENTO).click() 
-        cy.get(loc.CADASTRAR_DISCUPULOS.CLICAR_NA_OPCAO_PEQUENO_GRUPO).click()
-        cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_DISCIPULADORES).click()
-        cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_FLAVIA).click()
-        cy.get(loc.CADASTRAR_DISCUPULOS.ESCREVER_O_TELEFONE).type('11956892345')
-        cy.get(loc.CADASTRAR_DISCUPULOS.ESCREVER_A_ORIGEM).type('Convite de Flávia')
-        cy.get(loc.CADASTRAR_DISCUPULOS.CLICAR_NO_BOTAO_SALVAR).click()
+        cy.salvarDiscipulo('Gildo teste LT', 'Rua João adinaldo da Cunha', '11956892345', 'Convite de Flávia')
         cy.get(loc.CADASTRAR_DISCUPULOS.MENSAGEM_SUCESSO).should('contain', 'Discipulo adicionado com sucesso!')
     })
 })
---
+++ interessadosfront/interessadoCypress/cypress/support/commandsDiscipulo.js
@@ -0,0 +1,18 @@
+
+import loc from './locators'
+
+Cypress.Commands.add('salvarDiscipulo', (nome, endereco, telefone, origem) => {
+    cy.get(loc.CADASTRAR_DISCUPULOS.CLICAR_NO_CALENDARIO).click() 
+    cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_DATA).click()
+    cy.get(loc.CADASTRAR_DISCUPULOS.ESCREVER_NOME_DO_DISCIPULO).type(nome) 
+    cy.get(loc.CADASTRAR_DISCUPULOS.ESCREVER_O_ENDERECO_DO_DISCIPULO).type(endereco)
+    cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_TIPO_INTERESSE).click()
+    cy.get(loc.CADASTRAR_DISCUPULOS.CLICAR_NO_TIPO_INTERESSE).click()
+    cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_TIPO_ATENDIMENTO).click() 
+    cy.get(loc.CADASTRAR_DISCUPULOS.CLICAR_NA_OPCAO_PEQUENO_GRUPO).click()
+    cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_DISCIPULADORES).click()
+    cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_FLAVIA).click()
+    cy.get(loc.CADASTRAR_DISCUPULOS.ESCREVER_O_TELEFONE).type(telefone)
+    cy.get(loc.CADASTRAR_DISCUPULOS.ESCREVER_A_ORIGEM).type(origem)
+    cy.get(loc.CADASTRAR_DISCUPULOS.CLICAR_NO_BOTAO_SALVAR).click()
+})
