db6da344a32897bfd71980739aeb0eb88ed8e1c0
Wladimir Bandeira <tecnico100@gmail.com>
2022-01-11 14:32:10 -0300

    implementando locators

--- interessadosfront/interessadoCypress/cypress/integration/interessados/functional.spec.js
+++ interessadosfront/interessadoCypress/cypress/integration/interessados/functional.spec.js
@@ -1,13 +1,41 @@
 /// <reference types="cypress" />
 
+import loc from '../../support/locators'
 
 describe('acesso padrao',  () => {
 
     before(() => {
-        cy.visit('http://localhost:4200/discipulos')
+        cy.visit(loc.URL.LISTA_DISCIPULOS)
     })
 
-    it('teste qualquer', () => {
-        
+    it('teste de acesso ao menu', () => {
+        cy.get(loc.SISTEMA.MENU).click()
+        cy.get(loc.SISTEMA.DASHBOARD).should('have.text','Dashboard')
+        cy.get(loc.SISTEMA.FECHAR_MENU).click()
+    })
+
+    it('teste selecionamento de discipuladores', () => {
+        cy.visit(loc.URL.CADASTRO_DISCIPULOS)
+        cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_DISCIPULADORES).click()
+        cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_FLAVIA).click()
+        cy.get(loc.CADASTRAR_DISCUPULOS.VARIFICAR_FLAVIA_COMO_SELECIONADO).should('contain','Flávia')
+    })
+
+    it('salvando discipulo', () => {
+        cy.visit(loc.URL.CADASTRO_DISCIPULOS)
+        cy.get(loc.CADASTRAR_DISCUPULOS.CLICAR_NO_CALENDARIO).click() 
+        cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_DATA).click()
+        cy.get(loc.CADASTRAR_DISCUPULOS.ESCREVER_NOME_DO_DISCIPULO).type('Gildo teste LT') 
+        cy.get(loc.CADASTRAR_DISCUPULOS.ESCREVER_O_ENDERECO_DO_DISCIPULO).type('Rua João adinaldo da Cunha')
+        cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_TIPO_INTERESSE).click()
+        cy.get(loc.CADASTRAR_DISCUPULOS.CLICAR_NO_TIPO_INTERESSE).click()
+        cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_TIPO_ATENDIMENTO).click() 
+        cy.get(loc.CADASTRAR_DISCUPULOS.CLICAR_NA_OPCAO_PEQUENO_GRUPO).click()
+        cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_DISCIPULADORES).click()
+        cy.get(loc.CADASTRAR_DISCUPULOS.SELECIONAR_FLAVIA).click()
+        cy.get(loc.CADASTRAR_DISCUPULOS.ESCREVER_O_TELEFONE).type('11956892345')
+        cy.get(loc.CADASTRAR_DISCUPULOS.ESCREVER_A_ORIGEM).type('Convite de Flávia')
+        cy.get(loc.CADASTRAR_DISCUPULOS.CLICAR_NO_BOTAO_SALVAR).click()
+        cy.get(loc.CADASTRAR_DISCUPULOS.MENSAGEM_SUCESSO).should('contain', 'Discipulo adicionado com sucesso!')
     })
 })
---
+++ interessadosfront/interessadoCypress/cypress/support/locators.js
@@ -0,0 +1,42 @@
+const locators = {
+    CADASTRAR_DISCUPULOS: {
+        VARIFICAR_FLAVIA_COMO_SELECIONADO: '#pr_id_5_label',
+        CLICAR_NO_CALENDARIO: '.p-calendar > .p-inputtext',
+        SELECIONAR_DATA: '.p-datepicker-today > .p-ripple',
+        ESCREVER_NOME_DO_DISCIPULO: '.col-9 > .p-inputtext',
+        ESCREVER_O_ENDERECO_DO_DISCIPULO: '.col-12.col-3 > .p-inputtext',
+        SELECIONAR_TIPO_INTERESSE: '.col-12.col-6 > .p-element > .p-dropdown > .p-dropdown-trigger',
+        CLICAR_NO_TIPO_INTERESSE: '[ng-reflect-label="Frequenta a igreja e não há im"] > .p-ripple',
+        SELECIONAR_TIPO_ATENDIMENTO: ':nth-child(6) > .p-element > .p-dropdown > .p-dropdown-label',
+        CLICAR_NA_OPCAO_PEQUENO_GRUPO: '[ng-reflect-label="Pequeno Grupo"] > .p-ripple',
+        SELECIONAR_DISCIPULADORES: '.col-4 > .p-element > .p-dropdown > .p-dropdown-label',
+        SELECIONAR_FLAVIA: '[ng-reflect-label="Flávia"] > .p-ripple',
+        ESCREVER_O_TELEFONE: '.ng-untouched > .p-inputtext',
+        ESCREVER_A_ORIGEM: ':nth-child(10) > .p-inputtext',
+        CLICAR_NO_BOTAO_SALVAR: '[label="Salvar"]',
+        MENSAGEM_SUCESSO: '.p-toast-message-content'
+     },
+    DIT: {
+        BTN_INCLUIR_DIT: ':nth-child(1) > .btn',
+        SELECT_TIPO_PROCESSO: "//select2[@formcontrolname='idTipoProcesso']//span[@class='select2-selection__rendered']",
+        ESCREVER_TIPO_PROCESSO: '.select2-search__field',
+        BTN_TIPO_PROCESSO: "//li[. ='Doação']",
+        SELECT_MUNICIPIO: "//select2[@formcontrolname='codMunicipioProcesso']//span[@class='select2-selection__rendered']",
+        ESCREVER_O_MUNICIPIO: "//input[@class='select2-search__field']",
+        BTN_MUNICIPIO: "//li[. ='ALEGRETE']",
+        BTN_SUBMETER_INCLUCAO_DIT: '.btn-primary',
+        CONFIRMACAO_INCLUSAO: "//div[@class='toast-message ng-star-inserted']",
+        FECHAR_MENSAGEM_DE_SUCESSO: '.toast-close-button > .ng-tns-c9-0'
+    },
+    URL: {
+        LISTA_DISCIPULOS: 'http://localhost:4200/discipulos',
+        CADASTRO_DISCIPULOS: 'http://localhost:4200/discipulo/novo'
+    },
+    SISTEMA: {
+        MENU: 'p-button.p-element > .p-ripple',
+        FECHAR_MENU: '.p-sidebar-close-icon',
+        DASHBOARD: '.dashboard > span'
+    }
+}
+
+export default locators;
