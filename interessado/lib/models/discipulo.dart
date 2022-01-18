b5bcdd8a7fe2608aaf5c1e3b0b5e87dd0b5c2b0b
Wladimir Bandeira <tecnico100@gmail.com>
2022-01-18 08:55:00 -0300

    refatorando código flutter e implementando lista simples

---
+++ interessado/lib/models/discipulo.dart
@@ -0,0 +1,19 @@
+class Discipulo{
+  final String codigo;
+  final String nome;
+  final String logradouro;
+  final String telefone;
+  final String dataNascimento;
+  final String origem;
+
+
+  Discipulo({
+   required this.codigo,
+   required this.nome,
+   required this.logradouro,
+   required this.telefone,
+   required this.dataNascimento,
+   required this.origem
+  });
+
+}
